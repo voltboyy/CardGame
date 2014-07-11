4b986a5c27d6864a3e336db353e5390718c4b715
Junior <junior-verswijver@hotmail.com>
2014-07-11 20:53:17 +0200

    Client side data sending/retrieving basics added

--- Card Game/src/cardGame/Main.java
+++ Card Game/src/cardGame/Main.java
@@ -1,10 +1,21 @@
 package cardGame;
 
-public class Main {
+import java.io.*;
+import java.net.*;
 
-	public static void main(String[] args) {
-			System.out.println("Test");
-			System.out.println("And test again");
+public class Main {
+	
+	static Socket socket;
+	static DataInputStream in;
+
+	public static void main(String[] args) throws UnknownHostException, Exception {
+		System.out.println("Connecting...");
+		socket = new Socket("localhost", 7777);
+		System.out.println("Connection succesful!");
+		in = new DataInputStream(socket.getInputStream());
+		System.out.println("Receiving information...");
+		String test = in.readUTF();
+		System.out.println("Message from server: " + test);
 	}
 
 }
