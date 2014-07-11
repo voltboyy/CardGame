296a23dd7a2f79243044fe611510576c509368a8
Junior <junior-verswijver@hotmail.com>
2014-07-11 21:47:33 +0200

    Input and output for client, chat system

--- Card Game/src/cardGame/Main.java
+++ Card Game/src/cardGame/Main.java
@@ -2,20 +2,53 @@ package cardGame;
 
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 public class Main {
 	
 	static Socket socket;
 	static DataInputStream in;
+	static DataOutputStream out;
 
 	public static void main(String[] args) throws UnknownHostException, Exception {
 		System.out.println("Connecting...");
 		socket = new Socket("localhost", 7777);
 		System.out.println("Connection succesful!");
 		in = new DataInputStream(socket.getInputStream());
-		System.out.println("Receiving information...");
-		String test = in.readUTF();
-		System.out.println("Message from server: " + test);
+		out = new DataOutputStream(socket.getOutputStream());
+		Input input = new Input(in);
+		Thread thread = new Thread(input);
+		thread.start();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your name: ");
+		String name = sc.nextLine();
+		out.writeUTF(name);
+		while(true){
+			String sendMessage = sc.nextLine();
+			out.writeUTF(sendMessage);
+		}
 	}
 
+}
+
+class Input implements Runnable{
+
+	DataInputStream in;
+	
+	public Input(DataInputStream in){
+		this.in = in;
+	}
+	
+	public void run() {
+		while(true){
+			String message;
+			try {
+				message = in.readUTF();
+				System.out.println(message);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
 }
