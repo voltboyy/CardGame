113a15e7fe8ba61de1316944f195de1b8262fd2c
Junior <junior-verswijver@hotmail.com>
2014-07-11 22:57:04 +0200

    Added notes

--- Card Game/src/cardGame/Main.java
+++ Card Game/src/cardGame/Main.java
@@ -6,31 +6,34 @@ import java.util.Scanner;
 
 public class Main {
 	
+	//Global variables
 	static Socket socket;
 	static DataInputStream in;
 	static DataOutputStream out;
 
+	//Main method
 	public static void main(String[] args) throws UnknownHostException, Exception {
 		System.out.println("Connecting...");
-		socket = new Socket("localhost", 7777);
+		socket = new Socket("localhost", 7777); //Connect to specific server using specified port
 		System.out.println("Connection succesful!");
 		in = new DataInputStream(socket.getInputStream());
 		out = new DataOutputStream(socket.getOutputStream());
 		Input input = new Input(in);
 		Thread thread = new Thread(input);
 		thread.start();
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); //Reads input in console
 		System.out.println("Enter your name: ");
 		String name = sc.nextLine();
 		out.writeUTF(name);
 		while(true){
 			String sendMessage = sc.nextLine();
-			out.writeUTF(sendMessage);
+			out.writeUTF(sendMessage); //Sends message to server
 		}
 	}
 
 }
 
+//Separate class file
 class Input implements Runnable{
 
 	DataInputStream in;
@@ -43,8 +46,8 @@ class Input implements Runnable{
 		while(true){
 			String message;
 			try {
-				message = in.readUTF();
-				System.out.println(message);
+				message = in.readUTF(); //Receive message from server
+				System.out.println(message); //Write message in console
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
