04ae41be894e9b3c3985b5baa76da915e5c46876
Junior <junior-verswijver@hotmail.com>
2014-07-14 03:08:06 +0200

    Rewritten entire code to a java standalone with better performance handling and more possibilities for adding stuff in the near future, added gamepanel class file in the progress

---
+++ Card Game/src/cardGame/GamePanel.java
@@ -0,0 +1,408 @@
+package cardGame;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements Runnable{
+	
+	//This is needed for screen repainting, also known as double buffering
+	private Image dbImage;
+	private Graphics dbg;
+	
+	//getting proces speed from pc
+	static long getTickCount = java.lang.System.currentTimeMillis();
+	
+	static final int GWIDTH = Main.GWIDTH, GHEIGHT = Main.GHEIGHT;
+	static final Dimension panelSize = new Dimension(GWIDTH, GHEIGHT);
+	
+	//All variables that makes the game run good and smoothly, just ignore
+	private Thread game;
+	public Thread threadinput;
+	private volatile boolean running = false;
+	private long period = (long) (1*1000000); //ms -> nanoseconds
+	private static final int DELAYS_BEFORE_YIELD = 10;
+	
+	//booleans
+	boolean debug = false;
+	
+	boolean left, down, right, up;
+	
+	//Global variables
+	static Socket socket;
+	static DataInputStream in;
+	static DataOutputStream out;
+	
+	//unique id that server sends to client
+	int playerid;
+	
+	//local coordinates
+	int playerx;
+	int playery;
+	
+	//server coordinates van andere ids
+	int[] x = new int[10];
+	int[] y = new int[10];
+	
+	//Main method of this class file
+	public GamePanel(){
+		
+		addMouseListener(new MouseHandler());
+		addMouseMotionListener(new MouseHandler());
+		
+		//Load Images
+		//arrowNextIdle = new ImageIcon(getClass().getResource("/ArrowNextIdle.png")).getImage();
+		
+		//load sounds
+		//hoverSound = java.applet.Applet.newAudioClip(getClass().getResource("/hover.wav"));
+		
+		
+		//basic settings
+		setPreferredSize(panelSize);
+		setBackground(Color.DARK_GRAY);
+		setFocusable(true);
+		requestFocus(false);
+		
+		//Key presses and stuf are being tracked here
+		addKeyListener(new KeyAdapter(){
+			public void keyPressed(KeyEvent e){
+				if(e.getKeyCode() == KeyEvent.VK_D){
+					if(debug)
+						debug = false;
+					else
+						debug = true;
+				}
+				
+				if(e.getKeyCode() == KeyEvent.VK_LEFT){
+					left = true;
+				}
+				if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+					right = true;
+				}
+				if(e.getKeyCode() == KeyEvent.VK_UP){
+					up = true;
+				}
+				if(e.getKeyCode() == KeyEvent.VK_DOWN){
+					down = true;
+				}
+			}
+			
+			public void keyReleased(KeyEvent e){
+				if(e.getKeyCode() == KeyEvent.VK_LEFT){
+					left = false;
+				}
+				if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+					right = false;
+				}
+				if(e.getKeyCode() == KeyEvent.VK_UP){
+					up = false;
+				}
+				if(e.getKeyCode() == KeyEvent.VK_DOWN){
+					down = false;
+				}
+			}
+			
+			public void KeyTyped(KeyEvent e){
+				
+			}
+		});		
+	}
+	
+	//This is the main method that controls how the game runs.
+	public void run(){
+		long beforeTime, afterTime, diff, sleepTime, overSleepTime = 0;
+		int delays = 30;
+		
+		/*final int FRAMES_PER_SECOND = 25;
+	    final int SKIP_TICKS = 1000 / FRAMES_PER_SECOND;
+	    
+	    int next_game_tick = (int) getTickCount;*/
+		
+		while(true){ //"true" was "running", it works now but something might be broken now
+			
+			
+			if(right == true){
+				playerx += 1;
+			}
+			if(left == true){
+				playerx -= 1;
+			}
+			if(up == true){
+				playery -= 1;
+			}
+			if(down == true){
+				playery += 1;
+			}
+			if(right || left || up || down){ // "||" = of
+				try{
+					out.writeInt(playerid);
+					out.writeInt(playerx);
+					out.writeInt(playery);
+				}catch(Exception e){
+					System.out.println("Error sending Coordinates");
+				}
+			}
+			
+			
+			//A lot of bullshit that keeps game running on same speed but possibly with slower frame rate if pc is lagging
+			beforeTime = System.nanoTime();
+			gameUpdate();
+			gameRender();
+			paintScreen();
+			
+			afterTime = System.nanoTime();
+			diff = afterTime - beforeTime;
+			sleepTime = period - diff - overSleepTime;
+			//If the sleep time is between 0 and period, we can happily sleep ^^
+			if(sleepTime < period && sleepTime > 0){
+				try{	
+					game.sleep(sleepTime / 100000L);//default: 100000
+					threadinput.sleep(sleepTime / 100000L);
+					overSleepTime = 0;
+				}catch (InterruptedException ex) {
+					Logger.getLogger(GamePanel.class.getName()).log(Level.SEVERE, null, ex);
+				}
+			}
+			//The diff was greater than the period
+			else if(diff > period){
+				overSleepTime = diff - period;
+			}
+			//Accumulate the amount of delays and eventually yield
+			else if(++delays >= DELAYS_BEFORE_YIELD){
+				game.yield();
+				threadinput.yield();
+				delays = 0;
+				overSleepTime = 0;
+			}
+			//the loop took less time than expected, but we need to make up
+			//for the oversleeptime!
+			else{
+				overSleepTime = 0;
+			}
+			//Print out game stats
+			/*log(
+				"beforeTime:	" + beforeTime +"\n" +
+				"afterTime:	" + afterTime + "\n" +
+				"diff:		" + diff + "\n" +
+				"sleepTime:	" + sleepTime / 1000000L + "\n" +
+				"overSleepTime:	" + overSleepTime / 1000000L + "\n" +
+				"delays:		" + delays + "\n"
+			);*/
+		}
+	}
+	
+	//This method keeps the game running.
+	private void gameUpdate(){
+		if(game != null){ //! removed a part here, always check here for a possible fix!
+			//update game state
+			//if(level == 30){
+			//world1.moveMap();
+			//p1.update();
+			//}
+			//AchievementListener();
+			//if (world1.getSavedX() > 0){
+				//for (long stop=System.nanoTime()+TimeUnit.SECONDS.toNanos(2);stop>System.nanoTime();) {
+					//log(world1.getSavedX() + " " + world1.getSavedY());
+					//System.out.print(count + " ");
+				//}
+			//}
+			
+		}
+	}
+	
+	//This method makes sure that all textures are properly visualized
+	private void gameRender(){
+		if(dbImage == null){
+			dbImage = createImage(GWIDTH, GHEIGHT);
+			if(dbImage == null){
+				System.err.println("Why the fuck is that so called dbImage still null??");
+				return;
+			}else{
+				dbg = dbImage.getGraphics();
+			}
+		}
+		//Clear the screen
+		dbg.setColor(Color.DARK_GRAY);
+		dbg.fillRect(0, 0, GWIDTH, GHEIGHT);
+		//Draw Game elements
+		draw(dbg);
+		if(debug)
+			drawDebug(dbg);
+	}
+	
+	//This makes sure that the screen repaints itself with every tick
+	private void paintScreen(){
+		Graphics g;
+		try{
+			g = this.getGraphics();
+			if(dbImage != null && g != null){
+				g.drawImage(dbImage, 0, 0, null);
+			}
+			Toolkit.getDefaultToolkit().sync();
+			g.dispose();
+		}catch(Exception e){
+			System.err.println(e);
+		}
+	}
+	
+	//I have no clue, is necessary though
+	public void addNotify(){
+		super.addNotify();
+		startGame();
+	}
+	
+	private void startGame(){
+        if(game == null){ //removed something! Might cause bugs later! Remember!
+            
+        	//probably the most essential part of this entire class file
+            try{
+    			System.out.println("Connecting...");
+    			socket = new Socket("localhost", 7777); //Connect to specific server using specified port
+    			System.out.println("Connection succesful!");
+    			in = new DataInputStream(socket.getInputStream());
+    			playerid = in.readInt(); //Receiving id from server
+    			out = new DataOutputStream(socket.getOutputStream());
+    			Input input = new Input(in, this);
+    			threadinput = new Thread(input);
+    			threadinput.start();
+    			game = new Thread(this);
+                game.start();
+    		}catch(Exception e){
+    			System.out.println("Unable to start client");
+    		}
+            running = true;
+        }
+    }
+	
+	//Probably won't work as I deleted this "running" boolean on multiple places XD
+	public void stopGame(){
+		if(running){
+			running = false;
+		}
+	}
+	
+	//Easier sending stuff to console
+	private void log(String s){
+		System.out.println(s);
+	}
+	
+	//Draws content on screen!
+	public void draw(Graphics g){
+		g.setColor(Color.GRAY);
+        g.drawString("Options", 50, 50);
+		for(int i = 0; i < 10; i++){
+			g.drawOval(x[i], y[i], 5, 5);
+		}
+	}
+	
+	//Toggled by pressing the "D" key, shows some usefull values to help debugging, add as much as you want
+	public void drawDebug(Graphics g){
+		g.setFont(new Font("Arial", Font.PLAIN, 12));
+		g.setColor(Color.MAGENTA);
+		g.drawString("y: " + playery, 0, GHEIGHT - 30);
+		g.drawString("x: " + playerx, 0, GHEIGHT - 42);
+		//g.drawString("Saved Coord: "+world1.getSavedX()+" "+world1.getSavedY(), 0, GHEIGHT - 30);
+		//g.drawString("Count: "+count, 0, GHEIGHT - 42);
+	}
+	
+	//Updates the coordinates of other clients
+	public void updateCoordinates(int pid, int x2, int y2){
+		this.x[pid] = x2;
+		this.y[pid] = y2;
+	}
+	
+	//Easier declaring hitboxes, really useful for creating buttons
+	public boolean hitBox(Rectangle box, int x, int y){//This handles the detection of your cursor in rectangles, mostly buttons
+		if (x > box.x && x < box.x+box.width &&
+				y > box.y && y < box.y+box.height){
+			return true;
+		}
+		else
+			return false;
+	}
+	
+	//Easier filling of rectangles
+	public void fill(Rectangle box, Graphics g){//This fills the rectangle with your desired color
+		g.fillRect(box.x, box.y, box.width, box.height);
+	}
+	
+	//Ever wanted to do stuff with your mouse? Well you came to the rigth place! :p
+	public class MouseHandler extends MouseAdapter{
+		public void mouseMoved(MouseEvent e){
+			int mx = e.getX();
+			int my = e.getY();
+			
+			/*if(hitBox(achievementGet, mx, my))
+				AchievementGetHover = true;
+			else
+				AchievementGetHover = false;*/
+		}
+		public void mouseDragged(MouseEvent e){
+			
+		}
+		public void mouseEntered(MouseEvent e){
+			
+		}
+		public void mouseExited(MouseEvent e){
+			
+		}
+		public void mousePressed(MouseEvent e){
+			int mx = e.getX();
+			int my = e.getY();
+		}
+		public void mouseReleased(MouseEvent e){
+			
+		}
+		public void mouseClicked(MouseEvent e){
+			
+		}
+	}
+
+}
+
+//Seperate class file, could split it but... meh
+class Input implements Runnable{
+
+	DataInputStream in;
+	GamePanel client;
+	
+	public Input(DataInputStream in, GamePanel c){
+		this.in = in;
+		this.client = c;
+	}
+	
+	//Separate class file, listens to all information received from server
+	//and send this to gamepanel class file.
+	public void run() {
+		while(true){
+			try {
+				int playerid = in.readInt();
+				int x = in.readInt();
+				int y = in.readInt();
+				client.updateCoordinates(playerid, x, y); //This updates coordinates from other clients in gamepanel class file
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
+
--- Card Game/src/cardGame/Main.java
+++ Card Game/src/cardGame/Main.java
@@ -1,6 +1,7 @@
 package cardGame;
 
 import java.applet.Applet;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -8,154 +9,39 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
-public class Main extends Applet implements Runnable, KeyListener{
-	
-	//Global variables
-	static Socket socket;
-	static DataInputStream in;
-	static DataOutputStream out;
-	
-	//unique id that server sends to client
-	int playerid;
-	
-	//local coordinates
-	int playerx;
-	int playery;
-	
-	//server coordinates van andere ids
-	int[] x = new int[10];
-	int[] y = new int[10];
-	
-	//booleans
-	boolean left, down, right, up;
+import javax.swing.JFrame;
 
-	//Main method (changed to an initialize as it's an applet temporarily
-	public void init() {
-		setSize(100, 100); //screensize in px
-		addKeyListener(this); //Zorgt ervoor da die naar u toetsaanslagen kan 'luisteren'
-		try{
-			System.out.println("Connecting...");
-			socket = new Socket("localhost", 7777); //Connect to specific server using specified port
-			System.out.println("Connection succesful!");
-			in = new DataInputStream(socket.getInputStream());
-			playerid = in.readInt(); //Receiving id from server
-			out = new DataOutputStream(socket.getOutputStream());
-			Input input = new Input(in, this);
-			Thread thread = new Thread(input); //thread 1, listens to input
-			thread.start();
-			Thread thread2 = new Thread(this); //thread 2, runs this applet
-			thread2.start();
-		}catch(Exception e){
-			System.out.println("Unable to start client");
-		}
-	}
+public class Main extends JFrame{
 	
-	//Update van coordinaten van server van andere ids
-	public void updateCoordinates(int pid, int x2, int y2){
-		this.x[pid] = x2;
-		this.y[pid] = y2;
-	}
+	static int //deze twee heb ik gewoon overgenomen van ander programma, kunnen we nog volledig adjusten, don't worry :p
+    GWIDTH = 32*21, //adjust second number
+	GHEIGHT = 32*18; //adjust second number
+
+	Dimension screenSize = new Dimension(GWIDTH, GHEIGHT);
 	
-	//paints your stuff on screen
-	public void paint(Graphics g){
-		for(int i = 0; i < 10; i++){
-			g.drawOval(x[i], y[i], 5, 5);
-		}
-	}
-
-	//Makes this program run
-	public void run() {
-		while(true){
-			if(right == true){
-				playerx += 10;
-			}
-			if(left == true){
-				playerx -= 10;
-			}
-			if(up == true){
-				playery -= 10;
-			}
-			if(down == true){
-				playery += 10;
-			}
-			if(right || left || up || down){ // "||" = of
-				try{
-					out.writeInt(playerid);
-					out.writeInt(playerx);
-					out.writeInt(playery);
-				}catch(Exception e){
-					System.out.println("Error sending Coordinates");
-				}
-			}
-			repaint();
-			try {
-				Thread.sleep(400);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	public void keyPressed(KeyEvent e) { //What to do if a key is pressed
-		if(e.getKeyCode() == KeyEvent.VK_LEFT){
-			left = true;
-		}
-		if(e.getKeyCode() == KeyEvent.VK_RIGHT){
-			right = true;	
-		}
-		if(e.getKeyCode() == KeyEvent.VK_UP){
-			up = true;
-		}
-		if(e.getKeyCode() == KeyEvent.VK_DOWN){
-			down = true;
-		}
-	}
-
-	public void keyReleased(KeyEvent e) { //What to do if a key is released
-		if(e.getKeyCode() == KeyEvent.VK_LEFT){
-			left = false;
-		}
-		if(e.getKeyCode() == KeyEvent.VK_RIGHT){
-			right = false;	
-		}
-		if(e.getKeyCode() == KeyEvent.VK_UP){
-			up = false;
-		}
-		if(e.getKeyCode() == KeyEvent.VK_DOWN){
-			down = false;
-		}
-	}
-
-	public void keyTyped(KeyEvent e) { //Not sure...
+	
+	GamePanel gp;
+	
+	//This indicates that the entire program should start here
+	public static void main(String[] args){
 		
+		new Main();
+		
+	}
+
+	//Main method with some basic settings, not too hard
+	public Main() {
+		gp = new GamePanel();
+		setTitle("Card Game");
+		setSize(screenSize);
+		setResizable(false);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		add(gp);
 	}
 
 }
 
 //Separate class file, listens to all information received from server
 //and send this to main class file.
-class Input implements Runnable{
-
-	DataInputStream in;
-	Main client;
-	
-	public Input(DataInputStream in, Main c){
-		this.in = in;
-		this.client = c;
-	}
-	
-	public void run() {
-		while(true){
-			try {
-				int playerid = in.readInt();
-				int x = in.readInt();
-				int y = in.readInt();
-				client.updateCoordinates(playerid, x, y);
-				
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	
-}
